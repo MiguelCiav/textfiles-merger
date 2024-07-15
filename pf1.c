2e61301fcb8c24361f278c3c5fb876772fd22e15
MiguelCiav <miguelciavato@gmail.com>
2024-07-15 16:46:49 -0400

    Primeros avances en la fusion de archivos

--- pf1.c
+++ pf1.c
@@ -1,9 +1,12 @@
 #include "pf1.h"
 
 pthread_t *worker_threads;
-string **threads_strings;
 params_t *thread_params;
 stats_t *thread_stats;
+sem_t mutex;
+string **threads_strings;
+FILE **available_files;
+int max_available;
 
 void free_threads_data (int argc) {
 	free(threads_strings);
@@ -15,18 +18,66 @@ void free_threads_data (int argc) {
 void wait_for_workers (int argc) {
 	for (int i = 0; i < argc - 1; i++) {
 		pthread_join (worker_threads[i],NULL);
-		printf("Hilo %d finalizado\n", i);
 	}
 }
 
-void free_thread (params_t *params, int number_of_lines){
+void free_thread (params_t *params) {
+	int number_of_lines = thread_stats[params->id].number_of_lines;
 	for(int i = 0; i < number_of_lines; i++){
 		free(threads_strings[params->id][i]);
 	}
 	free(threads_strings[params->id]);
 }
 
-int comparator(const void* p, const void* q) { 
+void fill_merge_data(int argc, char* argv[]) {
+	available_files = (FILE**) malloc(sizeof(FILE*)*argc-1);
+	max_available = argc-1;
+	string url;
+	for(int i = 1; i < argc; i++){
+		url = strdup(argv[i]);
+		strcat(url,".sorted");
+		available_files[i - 1] = fopen(url,"r");
+	}
+}
+
+void merge_sorted_files(argc) {
+	while(max_available != 1){
+		if(max_available%2 == 0){
+			printf("Par (%d), creando %d hilos\n", max_available, max_available/2);
+			max_available = (max_available/2);
+		} else {
+			printf("Impar (%d), creando %d hilos\n", max_available, max_available/2);
+			max_available = (max_available/2) + 1;
+		}
+	}
+}
+
+int compare_by_length (const void* p, const void* q) {
+	string str_1 = *(const string*) p; 
+    string str_2 = *(const string*) q;
+	if(strlen(str_1) < strlen(str_2)) {
+		return -1;
+	}
+	if(strlen(str_1) > strlen(str_2)) {
+		return 1;
+	}
+	if(strlen(str_1) == strlen(str_2)){
+		return 0;
+	}
+}
+
+void save_thread_data (params_t *params) {
+	int number_of_lines = thread_stats[params->id].number_of_lines;
+	qsort((void*) threads_strings[params->id], number_of_lines, sizeof(string), compare_by_length);
+	thread_stats[params->id].shortest_line = threads_strings[params->id][0];
+	thread_stats[params->id].longest_line = threads_strings[params->id][number_of_lines - 1];
+	if(number_of_lines == 0) {
+		thread_stats[params->id].shortest_line = "\n";
+		thread_stats[params->id].longest_line = "\n";
+	}
+}
+
+int compare_by_alphabet(const void* p, const void* q) { 
     string str_1 = *(const string*) p; 
     string str_2 = *(const string*) q; 
 	
@@ -49,31 +100,31 @@ int comparator(const void* p, const void* q) {
 	}
 
 	/*
-    ( <0 ): Less than zero, if the element pointed by p1 goes before the element pointed by p2.
+    ( < 0 ): Less than zero, if the element pointed by p1 goes before the element pointed by p2.
     ( 0 ): Zero, if the element pointed by p1 is equivalent to the element pointed by p2.
-    ( >0 ): Greater than zero, if the element pointed by p1 goes after the element pointed by p2.
+    ( > 0 ): Greater than zero, if the element pointed by p1 goes after the element pointed by p2.
 	*/
 }
 
-void store_lines (params_t *params, int number_of_lines) {
+void store_lines (params_t *params) {
 	FILE *file;
 	size_t n = 0;
 	string sorted_filename = strdup(params->filename);
+	int number_of_lines = thread_stats[params->id].number_of_lines;
 	strcat(sorted_filename,".sorted");
 	file = fopen(sorted_filename, "w");
 	if(file == NULL){
 		printf("ERROR FATAL (store_lines): No se pudo abrir '%s'\n",sorted_filename);
 		pthread_exit(NULL);
 	}
-	qsort((void *) threads_strings[params->id], number_of_lines, sizeof(string), comparator);
+	qsort((void *) threads_strings[params->id], number_of_lines, sizeof(string), compare_by_alphabet);
 	for(int i = 0; i < number_of_lines; i++){
-		printf("Escribiendo: %s", threads_strings[params->id][i]);
 		fprintf(file,"%s",threads_strings[params->id][i]);
 	}
 	fclose(file);
 }
 
-void load_lines (params_t *params){
+void load_lines (params_t *params) {
 	FILE *file;
 	size_t n = 0;
 	char* line = NULL;
@@ -89,7 +140,7 @@ void load_lines (params_t *params){
 	free(line);
 }
 
-int count_lines (char* filename){
+int count_lines (char* filename) {
 	FILE *file;
 	size_t n = 0;
 	char* line = NULL;
@@ -111,9 +162,12 @@ void *worker_function (void *arg) {
 	params_t *params = (params_t*) arg;
 	int number_of_lines = count_lines(params->filename);
 	threads_strings[params->id] = malloc(sizeof(string)*number_of_lines);
+	thread_stats[params->id].number_of_lines = number_of_lines;
 	load_lines(params);
-	store_lines(params,number_of_lines);
-	free_thread(params,number_of_lines);
+	store_lines(params);
+	save_thread_data(params);
+	printf("This worker thread writes %d lines to \"%s.sorted\"\nlongest: %sshortest: %s", number_of_lines, params->filename, thread_stats[params->id].longest_line, thread_stats[params->id].shortest_line);
+	free_thread(params);
 }
 
 void init_worker_threads (int argc, char *argv[]) {
@@ -128,13 +182,15 @@ void init_threads_data (int argc) {
 	threads_strings = (string**) malloc(sizeof(string*)*(argc-1));
 	thread_params = (params_t*) malloc(sizeof(params_t)*(argc-1));
 	thread_stats = (stats_t*) malloc(sizeof(stats_t)*(argc-1));
-	worker_threads = (pthread_t*) malloc(sizeof(pthread_t) * (argc - 1));
+	worker_threads = (pthread_t*) malloc(sizeof(pthread_t)*(argc-1));
 }
 
 int main(int argc, char *argv[]) {
 	init_threads_data(argc);
 	init_worker_threads(argc,argv);
 	wait_for_workers(argc);
+	fill_merge_data(argc,argv);
+	merge_sorted_files();
 	free_threads_data(argc);
 	return 0;
 }
